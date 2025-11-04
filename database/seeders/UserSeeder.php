e7ba57f158ed7bdaff6d7bdb28371a67920a5faf
Stas <stanislav.pogorskii@gmail.com>
2025-11-05 01:53:59 +0400

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name'              => 'Stas Pogorskii',
+                'email'             => 'stanislav.pogorskii@gmail.com',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('Pa$$w0rd!'),
+            ],
+            [
+                'name'              => 'Kirill Pronin',
+                'email'             => 'procake@icloud.com',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('Pa$$w0rd!'),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
